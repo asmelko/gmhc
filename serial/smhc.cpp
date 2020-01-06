598ecde5adadfb942a044826471eb21341ad8363
Adam Šmelko <adamsmelko1@gmail.com>
2020-01-06 18:05:56 +0100

    add cluster merge

--- serial/smhc.cpp
+++ serial/smhc.cpp
@@ -4,80 +4,81 @@
 #include <lapacke.h>
 #include <stdexcept>
 
-#define LACH(x) if(x) throw std::runtime_error("LAPACK error")
+#define LACH(x) if (x) throw std::runtime_error("LAPACK error")
 
 using namespace clustering;
 
 smhc::smhc()
-	: kmeans_(1024, 1000) {}
+	: kmeans_(initial_cluster_count_, 1000), cluster_count_(initial_cluster_count_) {}
 
 void smhc::initialize(const float* data_points, size_t data_points_size, size_t data_point_dim)
 {
 	hierarchical_clustering::initialize(data_points, data_points_size, data_point_dim);
 	kmeans_.initialize(points, points_size, point_dim);
+	inverses_.resize(initial_cluster_count_ * point_dim * point_dim);
 }
 
 std::vector<pasgn_t> smhc::run()
 {
+	std::vector<pasgn_t> res;
 	auto [assignments, cetroids] = kmeans_.run();
+	centroids_ = std::move(cetroids);
+	assignments_ = std::move(assignments);
 
-	std::vector<matrix_t> inverses;
-
-	for (asgn_t i = 0; i < 1024; ++i)
-		inverses.emplace_back(
-			create_inverse_covariance_matrix(assignments.data(), i)
-		);
+	for (asgn_t i = 0; i < cluster_count_; ++i)
+		create_inverse_covariance_matrix(i, inverses_.data() + point_dim * point_dim * i);
 
 	float min_dist = -1.0f;
 	pasgn_t min_pair;
 
-	for (asgn_t i = 0; i < 1024; ++i)
+	while (cluster_count_ > 2)
 	{
-		for (asgn_t j = 0; j < 1024; ++j)
+		for (asgn_t i = 0; i < cluster_count_; ++i)
 		{
-			float tmp = mahalanobis_distance(i, j);
-
-			if (min_dist == -1 || tmp < min_dist)
+			for (asgn_t j = i; j < cluster_count_; ++j)
 			{
-				min_dist = tmp;
-				min_pair = std::make_pair(i, j);
-			};
+				float tmp = mahalanobis_distance(std::make_pair(i, j));
+
+				if (min_dist == -1 || tmp < min_dist)
+				{
+					min_dist = tmp;
+					min_pair = std::make_pair(i, j);
+				};
+			}
 		}
+
+		merge_clusters(min_pair);
+		res.push_back(min_pair);
 	}
 
-
-	return {};
+	return res;
 }
 
-void smhc::free()
-{
+void smhc::free() {}
 
-}
-
-float smhc::point_mean(const float* point)
+float smhc::point_mean(const float* point) const
 {
 	float res = 0;
 	for (size_t i = 0; i < point_dim; res += point[i++]);
 	return res / point_dim;
 }
 
-void smhc::point_subtract(const float* point, float scalar, float* dest)
+void smhc::point_subtract(const float* point, float scalar, float* dest) const
 {
 	cblas_scopy(1, point, 1, dest, 1);
 	for (size_t i = 0; i < point_dim; dest[i++] -= scalar);
 }
 
-smhc::matrix_t smhc::create_inverse_covariance_matrix(const asgn_t* assignments, const asgn_t cluster)
+void smhc::create_inverse_covariance_matrix(const asgn_t cluster, float* dest) const
 {
 	int dim = (int)point_dim; //to get rid of warnings
 
-	matrix_t icov;
-	icov.resize(point_dim * point_dim);
-	cblas_sscal(dim * dim, 0, icov.data(), 1); //zero out
+	float* icov = dest;
+	cblas_sscal(dim * dim, 0, icov, 1); //zero out
 
 	for (size_t i = 0; i < points_size; ++i)
 	{
-		if (assignments[i] != cluster) //ignore points in other clusters
+		if (assignments_[i] != cluster) //ignore points in other clusters
 			continue;
 
 		std::vector<float> tmp;
@@ -85,27 +86,44 @@ smhc::matrix_t smhc::create_inverse_covariance_matrix(const asgn_t* assignments,
 
 		point_subtract(points + i, point_mean(points + i), tmp.data());
 
-		cblas_ssyr(CblasRowMajor, CblasUpper, dim, 1, tmp.data(), 1, icov.data(), dim);
+		cblas_ssyr(CblasRowMajor, CblasUpper, dim, 1, tmp.data(), 1, icov, dim);
 	}
 
-	cblas_sscal(dim * dim, 1 / (float)dim, icov.data(), 1); //scale
+	cblas_sscal(dim * dim, 1 / (float)dim, icov, 1); //scale
 
 	std::vector<int> piv;
 	piv.resize(point_dim);
 
-	LACH(LAPACKE_ssytrf(LAPACK_ROW_MAJOR, 'U', dim, icov.data(), dim, piv.data())); //factorization
-	LACH(LAPACKE_ssytri(LAPACK_ROW_MAJOR, 'U', dim, icov.data(), dim, piv.data())); //inversion
-
-	return icov;
+	LACH(LAPACKE_ssytrf(LAPACK_ROW_MAJOR, 'U', dim, icov, dim, piv.data())); //factorization
+	LACH(LAPACKE_ssytri(LAPACK_ROW_MAJOR, 'U', dim, icov, dim, piv.data())); //inversion
 }
 
-float clustering::smhc::mahalanobis_distance(const asgn_t l_cluster, const asgn_t r_cluster)
+void smhc::create_cluster_cetroid(const asgn_t cluster, float* dest) const
+{
+	int dim = (int)point_dim;
+
+	float* cetroid = dest;
+	cblas_sscal(dim, 0, cetroid, 1); //zero out
+	size_t count = 0;
+
+	for (size_t i = 0; i < points_size; ++i)
+	{
+		if (assignments_[i] == cluster)
+		{
+			cblas_saxpy(dim, 1, points + point_dim * i, 1, cetroid, 1);
+			count++;
+		}
+	}
+	cblas_sscal(dim, 1 / (float)count, cetroid, 1);
+}
+
+float smhc::mahalanobis_distance(const pasgn_t cluster_pair) const
 {
 	int dim = (int)point_dim;
 	std::vector<float> diff;
 	diff.resize(point_dim);
-	float* l_data = centroids_.data() + l_cluster * point_dim;
-	float* r_data = centroids_.data() + r_cluster * point_dim;
+	const float* l_data = centroids_.data() + cluster_pair.first * point_dim;
+	const float* r_data = centroids_.data() + cluster_pair.second * point_dim;
 
 	cblas_scopy(dim, r_data, 1, diff.data(), 1);
 	cblas_saxpy(dim, -1, l_data, 1, diff.data(), 1);
@@ -113,11 +131,23 @@ float clustering::smhc::mahalanobis_distance(const asgn_t l_cluster, const asgn_
 	std::vector<float> tmp_res;
 	tmp_res.resize(point_dim);
 
-	cblas_ssymv(CblasRowMajor, CblasUpper, dim, 1, inverses_[l_cluster].data(), dim, diff.data(), 1, 0, tmp_res.data(), 1);
+	cblas_ssymv(CblasRowMajor, CblasUpper, dim, 1, inverses_.data() + point_dim * point_dim * cluster_pair.first, dim, diff.data(), 1, 0, tmp_res.data(), 1);
 	float distance = cblas_sdot(dim, diff.data(), 1, tmp_res.data(), 1);
 
-	cblas_ssymv(CblasRowMajor, CblasUpper, dim, 1, inverses_[r_cluster].data(), dim, diff.data(), 1, 0, tmp_res.data(), 1);
+	cblas_ssymv(CblasRowMajor, CblasUpper, dim, 1, inverses_.data() + point_dim * point_dim * cluster_pair.second, dim, diff.data(), 1, 0, tmp_res.data(), 1);
 	distance += cblas_sdot(dim, diff.data(), 1, tmp_res.data(), 1);
 
 	return distance / 2;
+}
+
+void smhc::merge_clusters(const pasgn_t cluster_pair)
+{
+	for (size_t i = 0; i < points_size; ++i)
+	{
+		if (assignments_[i] == cluster_pair.second)
+			assignments_[i] = cluster_pair.first;
+	}
+
+	create_inverse_covariance_matrix(cluster_pair.first, inverses_.data() + point_dim * point_dim * cluster_pair.first);
+	create_cluster_cetroid(cluster_pair.first, centroids_.data() + point_dim * cluster_pair.first);
 }
--- serial/smhc.hpp
+++ serial/smhc.hpp
@@ -7,10 +7,12 @@ namespace clustering {
 
 class smhc : public hierarchical_clustering<float>
 {
-	using matrix_t = std::vector<float>;
+	static constexpr size_t initial_cluster_count_ = 1024;
 	kmeans kmeans_;
-	std::vector<matrix_t> inverses_;
+	std::vector<float> inverses_;
 	std::vector<float> centroids_;
+	std::vector<asgn_t> assignments_;
+	size_t cluster_count_;
 public:
 	smhc();
 
@@ -19,12 +21,15 @@ public:
 	virtual std::vector<pasgn_t> run() override;
 
 	virtual void free() override;
-
 private:
-	float point_mean(const float* point);
-	void point_subtract(const float* point, float scalar, float* dest);
-	matrix_t create_inverse_covariance_matrix(const asgn_t* assignments, const asgn_t cluster);
-	float mahalanobis_distance(const asgn_t l_cluster, const asgn_t r_cluster);
+	float point_mean(const float* point) const;
+	void point_subtract(const float* point, float scalar, float* dest) const;
+
+	void create_inverse_covariance_matrix(const asgn_t cluster, float* dest) const;
+	void create_cluster_cetroid(const asgn_t cluster, float* dest) const;
+
+	float mahalanobis_distance(const pasgn_t cluster_pair) const;
+	void merge_clusters(const pasgn_t cluster_pair);
 };
 
 }
