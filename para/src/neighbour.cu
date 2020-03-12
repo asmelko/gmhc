5b20c0ec719beb2cf769c12e685085cc138e2c60
Adam Šmelko <adamsmelko1@gmail.com>
2020-03-12 08:38:41 +0100

    add neighbours martix kernel

--- para/src/neighbour.cu
+++ para/src/neighbour.cu
@@ -48,6 +48,7 @@ __device__ neighbour_array_t<N> merge_neighbours(const neighbour_array_t<N>* l_n
 	return tmp;
 }
 
+
 __inline__ __device__ float euclidean_norm(const float* l_point, const float* r_point, size_t dim)
 {
 	float tmp_sum = 0;
@@ -57,6 +58,17 @@ __inline__ __device__ float euclidean_norm(const float* l_point, const float* r_
 		tmp_sum += tmp * tmp;
 	}
 	return sqrtf(tmp_sum);
+}
+
+__inline__ __device__ void reduce_sum_warp(float* point, size_t dim)
+{
+	for (unsigned int offset = warpSize / 2; offset > 0; offset /= 2)
+	{
+		for (size_t i = 0; i < dim; ++i)
+		{
+			point[i] += __shfl_down_sync(__activemask(), point[i], offset);
+		}
+	}
 }
 
 
@@ -95,41 +107,187 @@ __inline__ __device__ void reduce_min_block(neighbour_array_t<N>* neighbours, ne
 }
 
 template <size_t N>
-__inline__ __device__ void reduce_min(neighbour_array_t<N>* neighbours, neighbour_array_t<N>* shared_mem, neighbour_array_t<N>* global_mem)
+__global__ void reduce(const neighbour_array_t<N>* neighbours, size_t to_reduce, size_t count, neighbour_array_t<N>* reduced)
 {
-	reduce_min_block(neighbours, shared_mem);
+	size_t in_block = blockDim.x / to_reduce;
 
-	if (threadIdx.x == 0)
+	for (size_t idx = threadIdx.x + blockIdx.x * blockDim.x; idx < count * warpSize; idx += blockDim.x * gridDim.x)
 	{
+		size_t block = idx / warpSize;
+		neighbour_array_t<N> local;
 
+		size_t nei = idx % warpSize;
+
+		if (nei < to_reduce)
+			local = neighbours[block * to_reduce + nei];
+		else
+			for (size_t i = 0; i < N; i++)
+				local.neighbours[i].distance = FLT_MAX;
+
+		for (nei += warpSize; nei < to_reduce; nei+=warpSize)
+			local = merge_neighbours(&local, neighbours + block * to_reduce + nei);
+
+		reduce_min_warp(&local);
+
+		if (threadIdx.x % warpSize == 0)
+			reduced[block] = local;
 	}
 }
 
 template <size_t N>
-__global__ void neighbours(const float* centroids, const float *const * inverses, size_t dim, size_t centroid_count, neighbour_array_t<N>* neighbours)
+__global__ void neighbours(const float* centroids, size_t dim, size_t centroid_count, neighbour_array_t<N>* neighbours)
 {
 	extern __shared__ float shared_mem[];
 
-	neighbour_array_t<N> local_neighbours;
-	for (size_t i = 0; i < N; ++i)
-		local_neighbours.neighbours[i].distance = FLT_MAX;
-
 	for (asgn_t x = 0; x < centroid_count; ++x)
 	{
-		if (inverses[x])
-			continue;
+		neighbour_array_t<N> local_neighbours;
+
+		for (size_t i = 0; i < N; ++i)
+			local_neighbours.neighbours[i].distance = FLT_MAX;
 
 		for (size_t i = threadIdx.x; i < dim; i += blockDim.x)
 			shared_mem[i] = centroids[x * dim + i];
 
+		__syncthreads();
+
 		for (asgn_t y = x + threadIdx.x; y < centroid_count; y += blockDim.x * gridDim.x)
 		{
-			if (inverses[y])
-				continue;
-
 			float dist = euclidean_norm(shared_mem, centroids + y * dim, dim);
 
 			add_neighbour(&local_neighbours, neighbour_t{ dist, y });
 		}
+
+		reduce_min_block(&local_neighbours, reinterpret_cast<neighbour_array_t<N>*>(shared_mem));
+
+		if (threadIdx.x == 0)
+			neighbours[gridDim.x * x + blockIdx.x] = local_neighbours;
+	}
+}
+
+template <size_t N>
+__global__ void neighbours_mat(const float* centroids, const float* const* inverses, size_t dim, size_t centroid_count, size_t start, neighbour_array_t<N>* neighbours)
+{
+	extern __shared__ float shared_mem[];
+
+	float* this_centroid = shared_mem;
+	float* this_icov = shared_mem + dim;
+	float* curr_centroid = this_icov + dim * dim;
+
+	for (asgn_t x = start; x < centroid_count; ++x)
+	{
+		neighbour_array_t<N> local_neighbours;
+
+		for (size_t i = 0; i < N; ++i)
+			local_neighbours.neighbours[i].distance = FLT_MAX;
+
+		for (size_t i = threadIdx.x; i < dim + dim * dim; i += blockDim.x)
+			if (i < dim)
+				shared_mem[i] = centroids[x * dim + i];
+			else
+				shared_mem[i] = inverses[x][i - dim];
+
+		__syncthreads();
+
+		for (asgn_t idx = threadIdx.x + blockIdx.x * blockDim.x; idx < (centroid_count - start) * warpSize; idx += blockDim.x * gridDim.x)
+		{
+			size_t y = idx / warpSize;
+
+			size_t warp_in_block = threadIdx.x / warpSize;
+			size_t thread_in_warp = threadIdx.x % warpSize;
+
+			for (size_t i = thread_in_warp; i < dim; i += warpSize)
+				curr_centroid[warp_in_block * dim + i] = centroids[y * dim + i];
+
+			float dist = euclidean_norm(this_centroid, curr_centroid + warp_in_block * dim, dim);
+
+			for (size_t i = thread_in_warp; i < dim; i += warpSize)
+				curr_centroid[warp_in_block * dim + i] -= this_centroid[i];
+
+			float tmp_point[MAX_DIM];
+			for (size_t i = 0; i < MAX_DIM; i++)
+				tmp_point[i] = 0;
+
+			for (size_t i = thread_in_warp; i < dim * dim; i += warpSize)
+			{
+				size_t mat_x = i / dim;
+				size_t mat_y = i % dim;
+
+				tmp_point[x] += this_icov[x + y * dim] * curr_centroid[warp_in_block * dim + x];
+			}
+
+			reduce_sum_warp(tmp_point, dim);
+
+			if (threadIdx.x == 0)
+			{
+				float tmp_res = 0;
+				for (size_t i = 0; i < dim; ++i)
+					tmp_res += tmp_point[i] * curr_centroid[warp_in_block * dim + i];
+
+				tmp_res = sqrtf(tmp_res);
+
+				dist = (dist + tmp_res) / 2;
+
+				add_neighbour(&local_neighbours, neighbour_t{ dist, y });
+			}
+		}
+
+		for (asgn_t idx = threadIdx.x + blockIdx.x * blockDim.x + (x + 1) * warpSize; idx < centroid_count * warpSize; idx += blockDim.x * gridDim.x)
+		{
+			size_t y = idx / warpSize;
+
+			size_t warp_in_block = threadIdx.x / warpSize;
+			size_t thread_in_warp = threadIdx.x % warpSize;
+
+			float* curr_icov = this_icov + dim * dim;
+			curr_centroid = curr_icov + dim * dim;
+
+			for (size_t i = threadIdx.x; i < dim*dim; i+=blockDim.x)
+				curr_icov[i] = inverses[y][i];
+
+			for (size_t i = thread_in_warp; i < dim; i += warpSize)
+				curr_centroid[warp_in_block * dim + i] = centroids[y * dim + i] - this_centroid[i];
+
+			float* icov = this_icov;
+			float distance = 0;
+			while (true)
+			{
+				float tmp_point[MAX_DIM];
+				for (size_t i = 0; i < MAX_DIM; i++)
+					tmp_point[i] = 0;
+
+				for (size_t i = thread_in_warp; i < dim * dim; i += warpSize)
+				{
+					size_t mat_x = i / dim;
+					size_t mat_y = i % dim;
+
+					tmp_point[x] += icov[x + y * dim] * curr_centroid[warp_in_block * dim + x];
+				}
+
+				reduce_sum_warp(tmp_point, dim);
+
+				if (threadIdx.x == 0)
+				{
+					float tmp_res = 0;
+					for (size_t i = 0; i < dim; ++i)
+						tmp_res += tmp_point[i] * curr_centroid[warp_in_block * dim + i];
+
+					distance += sqrtf(tmp_res);
+				}
+
+				if (icov == curr_icov)
+					break;
+				else
+					icov = curr_icov;
+			}
+
+			if (threadIdx.x == 0)
+				add_neighbour(&local_neighbours, neighbour_t{ distance / 2, y });
+		}
+
+		reduce_min_block(&local_neighbours, reinterpret_cast<neighbour_array_t<N>*>(shared_mem));
+
+		if (threadIdx.x == 0)
+			neighbours[gridDim.x * x + blockIdx.x] = local_neighbours;
 	}
 }
---
+++ para/tests/neighbour.hpp
@@ -0,0 +1,120 @@
+#include <gmock/gmock.h>
+#include <reader.hpp>
+
+#include <chrono>
+
+#include "serial_impl.hpp"
+
+size_t compute_output_in_line(size_t shared_size, size_t point_count)
+{
+	auto hsh = shared_size / 2;
+	return (point_count + hsh - 1) / hsh;
+}
+
+size_t compute_output_size(size_t shared_size, size_t point_count)
+{
+	auto in_line = compute_output_in_line(shared_size, point_count);
+	return ((in_line + 1) * in_line) / 2;
+}
+
+TEST(kernel, euclid_min_small)
+{
+	size_t shared_size = 20;
+	size_t output_size = 10;
+
+	std::string input = "3 32 -1.29547 8.00796 -7.49481 0.873838 0.59632 -1.51012 2.29091 0.944923 1.97487 -11.8492 0.220984 -3.32246 -3.18128 -0.856242 8.65632 -3.82426 3.51576 0.726751 0.468351 6.02061 1.30511 -3.4598 8.40714 -3.51013 0.875993 1.37086 -4.31532 5.93115 6.5827 3.40516 4.71282 4.14184 -2.94859 1.04552 4.30228 1.4721 -2.33436 -8.34392 -0.658711 7.88312 -3.95856 -4.61158 -5.23649 -2.20981 4.77685 -0.16196 0.051402 4.59879 -10.0206 0.902125 -0.0203374 1.64312 -2.10842 -1.20475 -12.092 0.0737986 2.18845 -1.17889 -11.8277 9.89593 3.83565 3.71229 -0.498549 0.436246 1.65099 4.69318 4.50654 4.72271 -3.79102 -5.22239 0.974885 -10.9422 5.59455 1.47331 5.72225 -1.47315 -2.90234 -7.91345 -9.92892 11.5448 -4.2494 -2.811 1.19261 -5.76345 -0.720766 16.3369 4.82052 0.6734 5.29015 1.93098 -2.19635 -2.60147 -3.42446 1.9895 -1.261 -2.28809";
+	auto data = clustering::reader::read_data_from_string<float>(input);
+
+	input_t cu_in;
+	clustering::chunk_t* cu_out;
+	clustering::chunk_t host_res;
+	float** cu_invs;
+	kernel_info kernel{ 3, 64, shared_size };
+
+	cu_in.count = data.points;
+	cu_in.dim = data.dim;
+
+	CUCH(cudaSetDevice(0));
+
+	CUCH(cudaMalloc(&cu_in.data, sizeof(float) * data.points * data.dim));
+	CUCH(cudaMalloc(&cu_out, sizeof(clustering::chunk_t) * output_size));
+	CUCH(cudaMalloc(&cu_invs, sizeof(float*) * data.points));
+
+	CUCH(cudaMemset(cu_invs, 0, sizeof(float*) * data.points));
+
+	CUCH(cudaMemcpy(cu_in.data, data.data.data(), sizeof(float) * data.points * data.dim, cudaMemcpyKind::cudaMemcpyHostToDevice));
+
+	run_euclidean_min(cu_in, cu_out, cu_invs, kernel);
+
+	CUCH(cudaGetLastError());
+	CUCH(cudaDeviceSynchronize());
+
+	CUCH(cudaMemcpy(&host_res, cu_out, sizeof(clustering::chunk_t), cudaMemcpyKind::cudaMemcpyDeviceToHost));
+
+	EXPECT_EQ(host_res.min_i, (clustering::asgn_t)6);
+	EXPECT_EQ(host_res.min_j, (clustering::asgn_t)29);
+}
+
+TEST(kernel, euclidean_min_big)
+{
+	///100 000 points with dim 15
+	auto data = clustering::reader::read_data_from_file<float>("big");
+
+	size_t shared_size = 700;
+	size_t output_size = compute_output_size(shared_size, data.points);
+
+	input_t cu_in;
+	clustering::chunk_t* cu_out;
+	clustering::chunk_t host_res;
+	float** cu_invs;
+	kernel_info kernel{ 50,  1024, shared_size };
+
+	cu_in.count = data.points;
+	cu_in.dim = data.dim;
+
+	CUCH(cudaSetDevice(0));
+
+	auto start = std::chrono::system_clock::now();
+
+	CUCH(cudaMalloc(&cu_in.data, sizeof(float) * data.points * data.dim));
+	CUCH(cudaMalloc(&cu_out, sizeof(clustering::chunk_t) * output_size));
+	CUCH(cudaMalloc(&cu_invs, sizeof(float*) * data.points));
+
+	CUCH(cudaMemset(cu_invs, 0, sizeof(float*) * data.points));
+
+	CUCH(cudaMemcpy(cu_in.data, data.data.data(), sizeof(float) * data.points * data.dim, cudaMemcpyKind::cudaMemcpyHostToDevice));
+
+	CUCH(cudaDeviceSynchronize());
+
+	auto end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "gpu prepare time: " << elapsed_seconds.count() << "\n";
+
+	start = std::chrono::system_clock::now();
+
+	run_euclidean_min(cu_in, cu_out, cu_invs, kernel);
+
+	CUCH(cudaGetLastError());
+	CUCH(cudaDeviceSynchronize());
+
+	end = std::chrono::system_clock::now();
+
+	elapsed_seconds = end - start;
+	std::cout << "gpu compute time: " << elapsed_seconds.count() << "\n";
+
+	CUCH(cudaGetLastError());
+	CUCH(cudaDeviceSynchronize());
+
+	CUCH(cudaMemcpy(&host_res, cu_out, sizeof(clustering::chunk_t), cudaMemcpyKind::cudaMemcpyDeviceToHost));
+
+	start = std::chrono::system_clock::now();
+	auto ser = serial_euclidean_min(data);
+	end = std::chrono::system_clock::now();
+
+	elapsed_seconds = end - start;
+	std::cout << "serial compute time: " << elapsed_seconds.count() << "\n";
+
+	EXPECT_EQ(host_res.min_i, ser.min_i);
+	EXPECT_EQ(host_res.min_j, ser.min_j);
+}
