e46407c04e08d09850f9d39d2890befaf3d5bbe6
Adam Šmelko <adamsmelko1@gmail.com>
2021-01-26 12:38:54 +0100

    initial

---
+++ Rgmhc/R/gmhclust.R
@@ -0,0 +1,15 @@
+gmhclust <- function(dataMatrix, threshold, kind) {
+
+	dim<-dim(dataMatrix)[1]
+	count<-dim(dataMatrix)[2]
+
+	merge<-matrix(0L,count-1,2)
+    height<-matrix(0.0,count-1,1)
+
+	if (!is.double(dataMatrix))
+		stop("argument x must be real")
+
+	res<-.C('gmhclust_C', dataMatrix, count, dim, threshold, kind, m=merge, h=height)
+
+	return(list(merge=res$m, height=res$h))
+}
---
+++ Rgmhc/src/gmhclust_C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "../para/include/gmhc.hpp"
+#include "gmhclust_C_export.h"
+
+using namespace clustering;
+
+extern "C"
+{
+    GMHCLUST_C_EXPORT void gmhclust_C(const double* data_points,
+        const int* data_points_size,
+        const int* data_point_dim,
+        const double* mahalanobis_threshold,
+        const int* subthreshold_kind,
+        int* merging,
+        double* heights)
+    {
+        gmhc gmhclust;
+
+        auto size = (csize_t)*data_points_size;
+        auto dim = (csize_t)*data_point_dim;
+        auto threshold = (csize_t)(size * *mahalanobis_threshold);
+        auto kind = (subthreshold_handling_kind)*subthreshold_kind;
+        
+        std::cout << "size " << *data_points_size << std::endl;
+        std::cout << "dim " << *data_point_dim << std::endl;
+        std::cout << "t " << *mahalanobis_threshold << std::endl;
+        std::cout << "k " << *subthreshold_kind << std::endl;
+        std::cout << "data[0] " << data_points[0] << std::endl;
+        std::cout << "data[1] " << data_points[1] << std::endl;
+
+        std::vector<float> data;
+        data.resize(size * dim);
+        for (size_t i = 0; i < data.size(); ++i)
+            data[i] = (float)data_points[i];
+
+        gmhclust.initialize(data.data(), size, dim, threshold, kind);
+
+        auto res = gmhclust.run();
+
+        for (size_t i = 0; i < res.size(); ++i)
+        {
+            merging[i] = res[i].first.first;
+            merging[i + size - 1] = res[i].first.second;
+            heights[i] = res[i].second;
+
+            std::cout << res[i].first.first << " " << res[i].first.second << " " << res[i].second << std::endl;
+        }
+
+        gmhclust.free();
+    }
+}
